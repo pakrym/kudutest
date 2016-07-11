2a85698446826e9ed12c70bfd918c77fbe6bcd76
Pavel Krymets <pavel@krymets.com>
2016-07-11 09:59:33 -0700

    Update dotent

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
-SET PATH=d:\home\site\stage2;%PATH%;
+SET DOTNET_CLI_CAPTURE_TIMING=1
+SET PATH=%DEPLOYMENT_SOURCE%\stage2;%PATH%;
 where dotnet
 cd src\live.asp.net
-dotnet restore
-dotnet build
-dotnet publish --no-build
+dotnet -v restore
+dotnet -v build
+dotnet -v publish --no-build
